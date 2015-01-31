885fcb0999a415a5cd99902f47f26a9c00fbefc2
Zane Wolfgang Pickett <Zane.Wolfgang.Pickett@Gmail.com>
2015-01-31 23:55:14 +0000

    create trigger

--- app/assets/javascripts/possum.js
+++ app/assets/javascripts/possum.js
@@ -2,21 +2,23 @@
 //= require_tree .
 
 (function(){
-
   var opossumProto = Object.create(HTMLElement.prototype);
-  
+
   opossumProto.createdCallback = function() {
+    var $this = $(this);
+
     if(this.hasAttribute('include')){
-      var element  = $(this);
-      var context  = $.getJSON( this.attributes.include.value + '.json' ).pipe( function( tasks, status, jqXHR ) { return tasks; } );
+      var context  = $.getJSON(this.attributes.include.value + '.json').pipe( function( tasks, status, jqXHR ) { return tasks; } );
       var template = $.get(this.attributes.include.value + '.html').pipe( function( tasks, status, jqXHR ) { return tasks; } );
 
       $.when( context, template ).done( function( context, template ) {
-        element.html(Handlebars.compile(template)(context));
+        $this.html(Handlebars.compile(template)(context));
+        $(document).trigger('possum:ready'); // only works for single object pages
       });
-
     }
   };
+
+  // $(document).trigger('possum:ready'); if all elements ready
   
   var opossum = document.registerElement('o-possum', { prototype: opossumProto });
 
